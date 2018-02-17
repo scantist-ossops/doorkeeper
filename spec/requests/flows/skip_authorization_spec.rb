2a2f9279fab2748742b92a8dec84d327004e6508
Justin Bull <justin@wealthsimple.com>
2018-02-17 15:42:51 -0500

    Add passing security spec for skipping auth screen

--- spec/requests/flows/skip_authorization_spec.rb
+++ spec/requests/flows/skip_authorization_spec.rb
@@ -5,7 +5,7 @@ feature 'Skip authorization form' do
     config_is_set(:authenticate_resource_owner) { User.first || redirect_to('/sign_in') }
     client_exists
     default_scopes_exist  :public
-    optional_scopes_exist :write
+    optional_scopes_exist :read, :write
   end
 
   context 'for previously authorized clients' do
@@ -16,12 +16,27 @@ feature 'Skip authorization form' do
 
     scenario 'skips the authorization and return a new grant code' do
       client_is_authorized(@client, @resource_owner, scopes: 'public')
-      visit authorization_endpoint_url(client: @client)
+      visit authorization_endpoint_url(client: @client, scope: 'public')
 
       i_should_not_see 'Authorize'
       client_should_be_authorized @client
       i_should_be_on_client_callback @client
       url_should_have_param 'code', Doorkeeper::AccessGrant.first.token
+    end
+
+    scenario 'does not skip authorization when application has greater scopes since last user consent' do
+      client_exists scopes: "public read"
+      client_is_authorized(@client, @resource_owner, scopes: 'public read')
+      visit authorization_endpoint_url(client: @client, scope: 'public read')
+
+      i_should_not_see 'Authorize'
+      client_should_be_authorized @client
+
+      @client.update! scopes: 'public read write'
+
+      visit authorization_endpoint_url(client: @client, scope: 'public read')
+      i_should_see 'Authorize'
+      expect(@client.access_grants.size).to eq(1) # that is, it did not increase
     end
 
     scenario 'does not skip authorization when scopes differ (new request has fewer scopes)' do
@@ -43,7 +58,7 @@ feature 'Skip authorization form' do
       access_grant_should_have_scopes :public
     end
 
-    scenario 'doesn not skip authorization when scopes are greater' do
+    scenario 'does not skip authorization when scopes are greater' do
       client_is_authorized(@client, @resource_owner, scopes: 'public')
       visit authorization_endpoint_url(client: @client, scope: 'public write')
       i_should_see 'Authorize'
